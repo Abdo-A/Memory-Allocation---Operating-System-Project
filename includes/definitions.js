fc1d3a1d7e9546d5689da46f00b9a7747ed16d1f
Abdo-A <abdopoetry@gmail.com>
2018-04-18 21:16:13 +0200

    Initial commit - Working on the files skeleton

---
+++ includes/definitions.js
@@ -0,0 +1,9 @@
+let body=document.getElementsByTagName("body")[0];
+
+let holesInput = document.getElementById("holesInput");
+let processesInput = document.getElementById("processesInput");
+
+let test = document.getElementById("test");
+
+let holesNumber=0;
+let processesNumber=0;
