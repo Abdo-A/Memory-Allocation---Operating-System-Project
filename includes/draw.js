a23548726f37f97e487213e21d4a8a7acd74f7b0
Abdo-A <abdopoetry@gmail.com>
2018-04-25 21:06:09 +0200

    Organized the functions

--- includes/draw.js
+++ includes/draw.js
@@ -1,12 +1,22 @@
 function draw(){
     answer.innerHTML=``;
     
-    answer.innerHTML+=`We will use <b>${type}</b>
-    <br>
-    Memory:<br>`;
-
     for(let i=0;i<memoryBlocksNumber;i++){
-        answer.innerHTML+=`<b>${memory[i].blockType}</b> ,size = ${memory[i].size} , from ${memory[i].startingAt} to ${memory[i].endingAt} `; 
-        answer.innerHTML+=`<br>`;
+        //answer.innerHTML+=`<b>${memory[i].blockType}</b> ,size = ${memory[i].size} , from ${memory[i].startingAt} to ${memory[i].endingAt} `; 
+        answer.innerHTML+=`<div
+            style="
+                width:100px;height:${memory[i].size}cm;background-color:#${Math.floor(Math.random()*(999999-111111+1))+111111}
+            "
+        >${memory[i].blockType}</div>`;
     }
+
+
+    // answer.innerHTML+=`We will use <b>${type}</b>
+    // <br>
+    // Memory:<br>`;
+
+    // for(let i=0;i<memoryBlocksNumber;i++){
+    //     answer.innerHTML+=`<b>${memory[i].blockType}</b> ,size = ${memory[i].size} , from ${memory[i].startingAt} to ${memory[i].endingAt} `; 
+    //     answer.innerHTML+=`<br>`;
+    // }
 }
